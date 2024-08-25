d87d1ea054cf5c296bb9a1cef9f8eb41c3bedd1e
unicodeveloper <prosperotemuyiwa@gmail.com>
2024-08-25 13:49:21 +0100

    add cache test

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\TalkController;
 use App\Jobs\AddDatatoDB;
 use App\Jobs\SendNotification;
 use Illuminate\Support\Facades\Bus;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -32,17 +33,28 @@ Route::get('/test', function() {
 
 Route::get('/jobs', function() {
 
-    // /**
-    //  * dispatch SendNotification and AddDataToDB Job
-    //  */
-    // dispatch(new SendNotification, new AddDatatoDB);
-
+    /**
+     * dispatch SendNotification and AddDataToDB Job
+     */
     Bus::chain([
         new AddDatatoDB,
         new SendNotification
     ])->dispatch();
 
     echo "Your jobs are being dispatching....";
+});
+
+Route::get('/cache', function() {
+
+    /**
+     * Testing your cache infra
+     */
+    echo "Storing inside the redis cache...";
+    Cache::store('redis')->put('bar', 'baz', 600); // 10 Minutes
+    
+    $value = Cache::get('bar');
+    echo "This is the result from the cache...{$value}";
+    
 });
 
 require __DIR__.'/auth.php';
