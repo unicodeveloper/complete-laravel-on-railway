14fb4e7984c4bd734bb4874a7851c2a217be8d9b
Prosper Otemuyiwa <prosperotemuyiwa@gmail.com>
2024-10-31 18:58:31 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # Make sure this file has executable permissions, run `chmod +x start.sh`
 # Run migrations, set up nginx conf and run nginx
-php artisan migrate && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
+php artisan migrate --force && node /assets/scripts/prestart.mjs /assets/nginx.template.conf /nginx.conf && (php-fpm -y /assets/php-fpm.conf & nginx -c /nginx.conf)
